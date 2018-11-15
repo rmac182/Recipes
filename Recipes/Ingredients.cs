28cc4acd1a9b542b651f201e772f46161f6c82d9
McPherson <ryan.mcpherson.182@gmail.com>
2018-11-15 13:05:08 -0600

    Have a question. Adding an item to a list of ingredients should add the amount and ingredient to the listbox. It is bound correctly because adding an item manually works. Adding it as a user using the add button method doesnt. see comments in code.

---
+++ Recipes/Ingredients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes
+{
+    public class Ingredient
+    {
+        public string IngName { get; set; }
+        public string Amount { get; set; }
+
+
+
+       
+
+    }
+}
--- Recipes/MainWindow.xaml.cs
+++ Recipes/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Recipes
 {
     /// <summary>
@@ -20,9 +21,81 @@ namespace Recipes
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        List<Ingredient> ingredients = new List<Ingredient>();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            FocusManager.SetFocusedElement(this, txtTitle);
+
+            // When I add an item to the list(ingredients) here, It shows up on the list box it is binded too but.... (see line )
+            ingredients.Add(new Ingredient { Amount = "AMOUNT", IngName = "INGREDIENT" });
+            
+            lbIngredients.ItemsSource = ingredients;
+            
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)txtTitle.Text != "" && (string)txtName.Text != "")
+            {
+                if (!(bool)chkBoxOption.IsChecked)
+                {
+                    recTitleName.Text = txtTitle.Text + "\n————————————————————————————————————————————————————————————\nBy: " + txtName.Text;
+                }
+                else
+                {
+                    recTitleName.Text = txtName.Text + "'s " + txtTitle.Text +
+                        "\n————————————————————————————————————————————————————————————\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Recipe title and Written By are required fields.");
+            }
+        }
+
+        private void BtnAddIngredient_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtAmount.Text != "" && txtIngredient.Text != "")
+            {
+                
+                 //...When the user adds am ingredient using this method here,  NOTHING!!!! Why???!!
+                ingredients.Add(new Ingredient { Amount = txtAmount.Text, IngName = txtIngredient.Text });
+                // I know it adds it because....
+                MessageBox.Show(ingredients[0].Amount + " " + ingredients[0].IngName + "\n" + ingredients[1].Amount + " " + ingredients[1].IngName);
+
+                txtIngredient.Text = "";
+                txtAmount.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Must Enter an Ingredient and an Amount.");
+            }
+        }
+
+        private void BtnEditIngredient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelIngredient_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+       
     }
 }
+
+                
+                
+
+
+
+
+               
+                  
